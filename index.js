2c56f45f9bd19e9f660dbb93842c851e6f7d068e
theerawut2539 <theerawut.s@psru.ac.th>
2019-12-06 15:04:01 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Theerawut Sringern");
+console.log("ธีรวุฒิ ศรีเงิน");
+//alert("กรุณากรอกตัวเลข")
+let firstName="Winai";
+let lastName="Gear";
+let age=5;
+let y=0.1;
+
+function equation(x,y,z)
+{
+    return ((x-1)+y)*z;
+}
+
+let result= equation(x,y,z); 
